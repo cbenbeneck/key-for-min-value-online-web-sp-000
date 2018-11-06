4debfd71078843d0799ffc5e817ee510a167452c
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-11-06 18:50:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  name_hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
+  name_hash.each do |name, value|
+    if value < lowest_value
+      lowest_value = value
+      lowest_key = name
     end
   end
   lowest_key
